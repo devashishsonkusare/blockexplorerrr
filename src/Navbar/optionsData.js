e6edfdc3d14e3c0c079c47ad62f37c6d282b0fcf
devashishsonkusare <devashish.s@krpyc.com>
2024-04-03 17:13:36 +0530

    Node Changes

--- src/Navbar/optionsData.js
+++ src/Navbar/optionsData.js
@@ -1,10 +1,6 @@
 const optionsData = [
     { value: "80001", symbol: "MATIC", name: "Polygon Mumbai", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_POLYGON_MUMBAI_RPC },
-    { value: "1", symbol: "ETH", name: "Ethereum Mainnet", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_ETHEREUM_MAINNET_RPC },
     { value: "11155111", symbol: "ETH", name: "Ethereum Sepolia", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_ETHEREUM_SEPOLIA_RPC },
-    { value: "137", symbol: "MATIC", name: "Polygon Mainet", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_POLYGON_MAINNET_RPC },
-    { value: "11155420", symbol: "ETH", name: "Optimism Sepolia", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_OPTIMISM_SEPOLIA_RPC },
-    { value: "42163", symbol: "ETH", name: "Arbitrum Georli", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_ARBITRUM_GOERLI_RPC },
     { value: "43113", symbol: "AVAX", name: "Avalance Fuji", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_AVALANCE_FUJI_RPC },
     { value: "296", symbol: "HBAR", name: "Hedera Testnet", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_HEDERA_TESTNET_RPC },
     { value: "84532", symbol: "ETH", name: "Base Sepolia", image: "https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg", rpc: process.env.REACT_APP_BASE_SEPOLIA_RPC },
